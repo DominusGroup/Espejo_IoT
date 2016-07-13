56bf5d1036657bdb90bcb9cef383c1a578832b02
Edgar Daniel <danichg94@gmail.com>
2016-07-13 14:32:07 -0600

    en español, threshold ajustable, timeout = 4

--- SpeechControl.py
+++ SpeechControl.py
@@ -6,16 +6,24 @@ import speech_recognition as sr
 
 # obtain audio from the microphone
 r = sr.Recognizer()
+
+# setting threshold
+r.energy_threshold = 300
+r.dynamic_energy_threshold = False
+
 with sr.Microphone() as source:
     print("Say something!")
-    audio = r.listen(source)
+    audio = r.listen(source, timeout = 4)
 
 # recognize speech using Google Speech Recognition
 try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said: " + r.recognize_google(audio))
+    audio2text = r.recognize_google(audio, key = None, language = "es-ES", show_all = False)
+    print("Google Speech Recognition thinks you said: " + audio2text)
+    ##b = audio.get_raw_data(convert_rate = None, convert_width = None)
+    
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
